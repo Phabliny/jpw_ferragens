6be8b120e0b3d8acba1aae076d29c49f389d1573
Phabliny <phablinymartins@gmail.com>
2022-06-29 14:49:48 -0300

    Alterações Adicionando o application-prod.properties

--- src/main/java/br/edu/iftm/jpw_ferragens/security/SecurityConfig.java
+++ src/main/java/br/edu/iftm/jpw_ferragens/security/SecurityConfig.java
@@ -46,8 +46,7 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
         http
                 .cors().and()
                 .authorizeHttpRequests()
-                .antMatchers(HttpMethod.POST,"/jpw/cliente", "/token").permitAll()
-                .antMatchers(HttpMethod.GET,"/jpw/clientes/export/excel").permitAll()
+                .antMatchers(HttpMethod.POST, "/jpw/cliente", "/token").permitAll()
                 .anyRequest().authenticated().and()
                 .csrf((csrf) -> csrf.ignoringAntMatchers("/token", "/jpw/cliente"))
                 .httpBasic(Customizer.withDefaults())
